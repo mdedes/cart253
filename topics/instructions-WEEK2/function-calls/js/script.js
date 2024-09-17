466386a146e64bd6452f622238bb7f28650edcc0
mdedes <marydedes@gmail.com>
2024-09-17 16:17:16 -0400

    Updated comments for The Blank Page project

--- topics/instructions-WEEK2/function-calls/js/script.js
+++ topics/instructions-WEEK2/function-calls/js/script.js
@@ -1,26 +1,35 @@
 /**
- * Function Calls
+ * The Blank Page
  * Mary Dedes
  * 
- * Working on the function calls examples.
+ * An exploration of teh existential angst of a novelsit confronting 
+ * the blank page while under huge pressure from their publisher.
+ * The program is non-interactive intentionally in order to simulate
+ * the writer's inability to get started on the project.
+ * 
+ * Uses: 
+ * p5.js
+ * https://p5js.org/
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates a 640x480 canvas for our masterpiece.
 */
 function setup() {
-// Once at the beginning of the program
+// Once at the beginning of the program: Create the canvas at a standard resolution
 createCanvas(640,480)
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws a blank piece of paper on a pink background.
 */
 function draw() {
 // Every frame
+// The pink desktop
 background(255, 100, 100)
+// The blank piece of paper
 rect(200, 80, 240, 320)
 }
