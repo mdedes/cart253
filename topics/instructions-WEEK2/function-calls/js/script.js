ab319b0918dbdf4b61c84c02cf9ac6ced621bfc5
mdedes <marydedes@gmail.com>
2024-09-17 15:48:16 -0400

    Added the template project for the function call project

---
+++ topics/instructions-WEEK2/function-calls/js/script.js
@@ -0,0 +1,26 @@
+/**
+ * Function Calls
+ * Mary Dedes
+ * 
+ * Working on the function calls examples.
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+// Once at the beginning of the program
+createCanvas(640,480)
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+// Every frame
+background(255, 100, 100)
+rect(200, 80, 240, 320)
+}
