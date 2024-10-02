cc3736044d05941f1f191f7a350c6dc75f21bc90
mdedes <marydedes@gmail.com>
2024-10-01 20:03:49 -0400

    Refactored the house code to organize it with functions

--- topics/instructions-WEEK2/starter-house/js/script.js
+++ topics/instructions-WEEK2/starter-house/js/script.js
@@ -19,13 +19,26 @@ function setup() {
 }
 
 /**
- * Draws a house
+ * Draws a house and its immediate environment
  */
 function draw() {
-    // The sky
-    background(150, 200, 250);
+    drawSky()
+    drawCloud()
+    drawGround()
+    drawHouse()
+}
 
-    // A cloud
+/**
+ * Displays a blue sky.
+ */
+function drawSky() {
+    background(150, 200, 250);
+}
+
+/**
+ * Draws a fluffy white cloud.
+ */
+function drawCloud() {
     push();
     noStroke();
     // Note: using a single number for a colour will be greyscale
@@ -38,30 +51,57 @@ function draw() {
     ellipse(190, 130, 60, 60);
     ellipse(220, 120, 60, 60);
     pop();
+}
 
+/**
+ * Draws the ground
+ */
+function drawGround() {
     // The ground
     push();
     noStroke();
     fill(200);
     rect(0, 400, 640, 480);
     pop();
+}
 
-    // The main body of the house
+/**
+ * Draws a house.
+ */
+function drawHouse() {
+    drawHouseBody()
+    drawHouseRoof()
+    drawHouseWindow()
+    drawHouseEntrance()
+}
+
+/**
+ * Draws the main body of the house
+ */
+function drawHouseBody() {
     push();
     noStroke();
     fill(250, 250, 200);
     rect(200, 240, 280, 180);
     pop();
+}
 
-    // The roof
+/**
+ * Draws the house roof.
+ */
+function drawHouseRoof() {
     push();
     noStroke();
     // You can also write colors in hex code in quote marks
     fill("#dc143c");
     triangle(180, 240, 340, 120, 500, 240);
     pop();
+}
 
-    // A window
+/**
+ * Draws the house's window
+ */
+function drawHouseWindow() {
     push();
     // You can also write colour names from the CSS standard in quotes
     // https://www.w3.org/wiki/CSS/Properties/color/keywords
@@ -70,17 +110,31 @@ function draw() {
     fill("blanchedalmond");
     rect(220, 260, 80, 80);
     pop();
+}
 
-    // An entrace
+/**
+ * Draws house entrance
+ */
+function drawHouseEntrance() {
+    drawHouseEntranceDoor()
+    drawHouseEntranceDoorknob()
+}
 
-    // The door
+/**
+ * Draws house entrance door.
+ */
+function drawHouseEntranceDoor() {
     push();
     noStroke();
     fill(0, 128, 0);
     rect(320, 300, 80, 120);
     pop();
+}
 
-    // The doorknob
+/**
+ * Draws house entrance doorknob.
+ */
+function drawHouseEntranceDoorknob() {
     push();
     noStroke();
     fill(255, 215, 0);
