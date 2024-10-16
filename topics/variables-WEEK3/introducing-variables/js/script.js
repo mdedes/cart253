ef145f06826b7455eedb9bcc7074aea3c36317b7
mdedes <marydedes@gmail.com>
2024-10-16 18:35:38 -0400

    Started working on introducing variables project

--- topics/variables-WEEK3/introducing-variables/js/script.js
+++ topics/variables-WEEK3/introducing-variables/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Author Name
+ * Introducing Variables
+ * Mary Dedes
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Learning what a variable is and does.
  */
 
 "use strict";
