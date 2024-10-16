85ff5baed7b4c1fc126438a3851b8ceb0a4c7672
mdedes <marydedes@gmail.com>
2024-10-16 18:54:27 -0400

    Drew a hole in a piece of cheese

--- topics/variables-WEEK3/creating-variables/js/script.js
+++ topics/variables-WEEK3/creating-variables/js/script.js
@@ -1,24 +1,32 @@
 /**
- * Title of Project
- * Author Name
+ * Creating Variables
+ * Mary Dedes
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Experimenting with creating variables.
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates canvas.
 */
 function setup() {
+    createCanvas(480, 480);
 
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws a hole in a piece of cheese.
 */
 function draw() {
+    background(255, 255, 0)
+
+    // The hole
+    push();
+    noStroke();
+    fill(0);
+    ellipse(140, 175, 180);
+    pop();
 
 }
