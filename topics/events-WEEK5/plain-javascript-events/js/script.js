aeada9e886bf69ed5bd9f015d54311b81a047611
mdedes <marydedes@gmail.com>
2024-10-17 17:33:56 -0400

    Started plain-javascript-events project in week5

---
+++ topics/events-WEEK5/plain-javascript-events/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Plain JavaScript Events
+ * Mary Dedes
+ * 
+ * Experimenting with event handling in Plain JavaScript
+ */
+
+"use strict";
+
+/**
+ * 
+*/
+function setup() {
+
+}
+
+
+/**
+ * 
+*/
+function draw() {
+
+}
