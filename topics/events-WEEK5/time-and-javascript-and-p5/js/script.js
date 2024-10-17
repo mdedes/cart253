689b7d669bed7722ec17503a4703b59349c0f028
mdedes <marydedes@gmail.com>
2024-10-17 14:44:55 -0400

    Created a time example

--- topics/events-WEEK5/time-and-javascript-and-p5/js/script.js
+++ topics/events-WEEK5/time-and-javascript-and-p5/js/script.js
@@ -8,10 +8,17 @@
 
 "use strict";
 
+const ball = {
+    x: 0,
+    y: 200,
+    size: 50
+}
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    createCanvas(400, 400)
 
 }
 
@@ -20,5 +27,12 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0);
 
+    ball.x += 1;
+
+    push();
+    noStroke();
+    ellipse(ball.x, ball.y, ball.size);
+    pop();
 }
